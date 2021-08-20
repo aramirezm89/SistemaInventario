9f34790f74f40ca57eae8ac63bba478e9b7a4e6c
antonio isaac ramirez monsalve <metal_invaders_21@hotmail.com>
2021-08-20 10:55:01 -0400

    VistaIndexOrdenes

---
+++ SistemaInventario/Areas/Admin/Controllers/OrdenController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrdenController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public OrdenController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region
+        [HttpGet]
+        public IActionResult ObtenerOrdenLista()
+        {
+            IEnumerable<Orden> ordenLista;
+            ordenLista = _unidadTrabajo.Orden.ObtenerTodos(incluirPropiedades: "UsuaioAplicacion");
+
+            return Json(new { data = ordenLista });
+        }
+        #endregion
+    }
+}
